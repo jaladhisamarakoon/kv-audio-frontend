6915591d8729eb8309bf0e97f6ddd8ef7aa4e18b
JaladhiSamarakoon <it22076120@my.sliit.lk>
2025-03-27 11:04:43 +0530

    day 10

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
 
 import './App.css'
+import { GoGraph } from "react-icons/go";
+import { FaRegBookmark } from "react-icons/fa";
+import { MdOutlineSpeaker } from "react-icons/md";
+import { FaRegUser } from "react-icons/fa";
 import ProductCard from './compnonents/productCard'
 
 function App() {
 
 
   return (
-    <div>
-  <ProductCard name="Audio setup" price = "3250/-" description ="dsdfsed sdsfe sd def sw"
-  img = "https://img.drz.lazcdn.com/g/kf/S6cfb0ee8d9f74ebb9660fd04b3622e364.jpg_400x400q75.avif"/>
+    <div className='w-full h-screen flex'>
+      <div className='w-[300px] h-full bg-green-200'>
+        <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
+         <GoGraph/> Dashboard
+        </button>
 
-  <ProductCard name ="jbl speaker" price = "2374/-" description = "JBL Bluetooth Speaker Mini Wireless Speaker Loud Stereo Bluetooth Subwoofer M3 Mini Speaker Support USB TF AUX MIC Rechargeable Portable Party Box with 6 Month Warranty and Free Shipping"
-  img = "https://img.drz.lazcdn.com/static/lk/p/a0d402ebb09ecb747ab10db9aebdb123.jpg_400x400q75.avif"/>
- 
+        <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center '>
+          <FaRegBookmark/>Bookings
+        </button>
+
+        <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center '>
+        <MdOutlineSpeaker/> Items
+        </button>
+        <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center '>
+          <FaRegUser/>Users
+        </button>
+        
+      </div>
+
+      <div className='w-full bg-red-900'></div>
+      
     </div>
   )
 }
