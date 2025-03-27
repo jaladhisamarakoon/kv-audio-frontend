98b699a43dbc2a3bc4342c5875ec3e5ad4243195
JaladhiSamarakoon <it22076120@my.sliit.lk>
2025-03-27 08:12:35 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import ProductCard from './compnonents/productCard'
+
+function App() {
+
+
+  return (
+    <div>
+  <ProductCard name="Audio setup" price = "3250/-" description ="dsdfsed sdsfe sd def sw"
+  img = "https://img.drz.lazcdn.com/g/kf/S6cfb0ee8d9f74ebb9660fd04b3622e364.jpg_400x400q75.avif"/>
+
+  <ProductCard name ="jbl speaker" price = "2374/-" description = "JBL Bluetooth Speaker Mini Wireless Speaker Loud Stereo Bluetooth Subwoofer M3 Mini Speaker Support USB TF AUX MIC Rechargeable Portable Party Box with 6 Month Warranty and Free Shipping"
+  img = "https://img.drz.lazcdn.com/static/lk/p/a0d402ebb09ecb747ab10db9aebdb123.jpg_400x400q75.avif"/>
+ 
+    </div>
+  )
+}
+
+export default App
